223f48b60316d5567e395f4ab3d891f52af4f1b5
iwanli <ibanya@126.com>
2017-12-13 21:49:27 +0800

    自定义分页

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -131,23 +131,8 @@
 
                         </div>
                         <!-- end: Blog -->
-                        {{$articles->links()}}
                         <!-- Pagination -->
-                        <div class="pagination pagination-simple">
-                            <ul>
-                                <li>
-                                    <a href="#" aria-label="Previous"> <span aria-hidden="true"><i class="fa fa-angle-left"></i></span> </a>
-                                </li>
-                                <li><a href="#">1</a> </li>
-                                <li><a href="#">2</a> </li>
-                                <li class="active"><a href="#">3</a> </li>
-                                <li><a href="#">4</a> </li>
-                                <li><a href="#">5</a> </li>
-                                <li>
-                                    <a href="#" aria-label="Next"> <span aria-hidden="true"><i class="fa fa-angle-right"></i></span> </a>
-                                </li>
-                            </ul>
-                        </div>
+                        {{$articles->links('pagination::blog')}}
                         <!-- end: Pagination -->
 
                     </div>
